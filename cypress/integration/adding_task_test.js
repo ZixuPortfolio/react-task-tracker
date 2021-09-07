ad171ed58c7f9670e880aafe7cd1e323c562ea2f
ZixuPortfolio <tanzix9498@gmail.com>
2021-09-07 08:47:05 +0800

    uncommented first test

--- cypress/integration/adding_task_test.js
+++ cypress/integration/adding_task_test.js
@@ -1,38 +1,38 @@
 import Button from "../../src/components/Button"
 
 
-// describe('Add Task Test', () => {
-//   before(() => {
-//     cy.visit('http://localhost:3000/')
-//   })
+describe('Add Task Test', () => {
+  before(() => {
+    cy.visit('http://localhost:3000/')
+  })
 
-//   it('Can Open Add Task Form via Add Button', () => {
-//     cy.get('#FormOpenClose').click()
-//   })
+  it('Can Open Add Task Form via Add Button', () => {
+    cy.get('#FormOpenClose').click()
+  })
 
-//   it('Can Add Task Details', () => {
-//     cy.get('.add-form').within(() => {
-//       cy.get('#TaskTitle').type('Take a walk')
-//       cy.get('#TaskDateTime').type('Sep 6th at 7.00pm')
-//       cy.get('#TaskReminder').check()
-//     })
-//   })
+  it('Can Add Task Details', () => {
+    cy.get('.add-form').within(() => {
+      cy.get('#TaskTitle').type('Take a walk')
+      cy.get('#TaskDateTime').type('Sep 6th at 7.00pm')
+      cy.get('#TaskReminder').check()
+    })
+  })
 
-//   it('Can Sumbit New Task', () => {
-//     cy.get('.add-form').submit()
-//   })
+  it('Can Sumbit New Task', () => {
+    cy.get('.add-form').submit()
+  })
 
-//   it('Can Close Add Task Form via Close Button', () => {
-//     cy.get('#FormOpenClose').click()
-//   })
+  it('Can Close Add Task Form via Close Button', () => {
+    cy.get('#FormOpenClose').click()
+  })
 
-//   it('can remove first task out of the four tasks', () => {
-//     cy.get('.task').should('have.length', 4)
-//     cy.get('.task').first().get('#remove').click()
-//     cy.get('.task').should('have.length', 3)
-//   })
+  it('can remove first task out of the four tasks', () => {
+    cy.get('.task').should('have.length', 4)
+    cy.get('.task').first().get('#remove').click()
+    cy.get('.task').should('have.length', 3)
+  })
 
-// })
+})
 
 describe('State Checking', () => {
   beforeEach(() => {
